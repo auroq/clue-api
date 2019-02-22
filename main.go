03379981a230e52100c4c5cdc0e7e1f20a0ad667
Parker Johansen <parker-johansen@pluralsight.com>
2019-02-22 16:39:07 -0700

    Add basic crud api for players

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 	"time"
 )
+
+type Player struct {
+	ID string `json:"id,omitempty"`
+	human bool `json:"human,omitempty"`
+	name string `json:"name,omitempty"`
+	dateCreated time.Time `json:"dateCreated,omitempty"`
+	dateModified time.Time `json:"dateModified,omitempty"`
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -18,15 +27,41 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func GetPlayers(w http.ResponseWriter, r *http.Request) {}
-func GetPlayer(w http.ResponseWriter, r *http.Request) {}
-func CreatePlayer(w http.ResponseWriter, r *http.Request) {}
-func DeletePlayer(w http.ResponseWriter, r *http.Request) {}
+var players []Player
 
-type Player struct {
-	id string `json:"id,omitempty"`
-	human bool `json:"human,omitempty"`
-	name string `json:"name,omitempty"`
-	dateCreated time.Time `json:"dateCreated,omitempty"`
-	dateModified time.Time `json:"dateModified,omitempty"`
+func GetPlayers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(players)
+}
+
+func GetPlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, player := range players {
+		if player.ID == params["id"] {
+			json.NewEncoder(w).Encode(player)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Player{})
+}
+
+func CreatePlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var player Player
+	_ = json.NewDecoder(r.Body).Decode(&player)
+	player.ID = params["id"]
+	player.dateCreated = time.Now()
+	player.dateModified = time.Now()
+	players = append(players, player)
+	json.NewEncoder(w).Encode(players)
+}
+
+func DeletePlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for i, player := range players {
+		if player.ID == params["ID"] {
+			players = append(players[:i], players[i+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(players)
+	}
 }
