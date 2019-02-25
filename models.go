182ae45d0beb993b65bbda4eb00bae84af01af90
Parker Johansen <johansen.parker@gmail.com>
2019-02-24 23:02:27 -0700

    Add struct for game

--- models.go
+++ models.go
@@ -5,9 +5,17 @@ import (
 )
 
 type Player struct {
-	ID interface{} `bson:"_id,omitempty", json:"id,omitempty"`
+	ID interface{} `bson:"_id,omitempty" json:"id,omitempty"`
 	Name string `json:"name"`
 	Human bool `json:"human"`
+	DateCreated time.Time `json:"dateCreated,omitempty"`
+	DateModified time.Time `json:"dateModified,omitempty"`
+}
+
+type Game struct {
+	ID interface{} `bson:"_id,omitempty" json:"id,omitempty"`
+	Name string `json:"name"`
+	Players []Player `json:"players"`
 	DateCreated time.Time `json:"dateCreated,omitempty"`
 	DateModified time.Time `json:"dateModified,omitempty"`
 }
